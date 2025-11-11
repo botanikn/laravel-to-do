9ab19d96c5a0a8df4ff8c763ee6a50161d763789
yusef.alassar <yusef.alassar@simpleone.ru>
2025-11-11 21:05:20 +0300

    api routed upgraded

--- routes/api.php
+++ routes/api.php
@@ -1,38 +1,33 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
+declare(strict_types=1);
+
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskTagController;
+use App\Http\Middleware\CheckApiToken;
+use Illuminate\Support\Facades\Route;
 
+// Public routes
 Route::post('/login', [LoginController::class, 'login']);
-
 Route::post('/register', [RegisterController::class, 'register']);
 
-Route::middleware(\App\Http\Middleware\CheckApiToken::class)->group(function () {
+// Protected routes (require API token)
+Route::middleware(CheckApiToken::class)->group(function () {
+    // Task routes
+    Route::apiResource('tasks', TaskController::class)
+        ->only(['index', 'store', 'show', 'update', 'destroy']);
 
-    // Эндпоинты для task
-    Route::resource('tasks', TaskController::class)
-    ->except(['show', 'create', 'edit']);
+    // Tag routes
+    Route::apiResource('tags', TagController::class)
+        ->only(['index', 'store', 'show', 'update', 'destroy']);
 
-    Route::get('tasks/{id}', [TaskController::class, 'findById'])->name('tasks.show');
-
-    // Эндпоинты для tag
-    Route::resource('tags', TagController::class)
-    ->except(['show', 'create', 'edit']);
-
-    Route::get('tags/{id}', [TagController::class, 'findById'])->name('tags.show');
-
-    // Route::get('/tags', [TagController::class, 'index']);
-    // Route::get('/tag/{id}', [TagController::class, 'findById']);
-    // Route::post('/tag', [TagController::class, 'store']);
-    // Route::put('/tag/{id}', [TagController::class, 'update']);
-    // Route::delete('/tag/{id}', [TagController::class, 'destroy']);
-
-    // Эндпоинты для task_tag
-    Route::get('/task_tag/{id}', [TaskTagController::class, 'findTasksByTagId']);
-    Route::post('/task_tag', [TaskTagController::class, 'addTagToTask']);
-    Route::delete('/task_tag', [TaskTagController::class, 'deleteTagFromTask']);
+    // Task-Tag relationship routes
+    Route::prefix('task_tag')->group(function () {
+        Route::get('/{tagId}/tasks', [TaskTagController::class, 'findTasksByTagId']);
+        Route::post('/', [TaskTagController::class, 'addTagToTask']);
+        Route::delete('/', [TaskTagController::class, 'deleteTagFromTask']);
+    });
 });
