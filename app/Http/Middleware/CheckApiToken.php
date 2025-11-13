d9128f061c3c0c716afd33d55aa5b20152a5af68
yusef.alassar <yusef.alassar@simpleone.ru>
2025-11-13 10:24:07 +0300

    added Resource answer

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use App\Models\User;
 use App\Constants\HttpStatus;
+use App\Http\Resources\ErrorResource;
 
 class CheckApiToken
 {
@@ -13,37 +14,25 @@ class CheckApiToken
         $authHeader = $request->header('Authorization');
 
         if (!$authHeader) {
-            return response()->json(
-                ['message' => 'Не предоставлен токен'],
-                HttpStatus::UNAUTHORIZED
-            );
+            return new ErrorResource(message: 'Не предоставлен токен', statusCode: HttpStatus::UNAUTHORIZED);
         }
 
         // Проверяем, что заголовок не начинается с 'Bearer '
         if (!str_starts_with($authHeader, 'Bearer ')) {
-            return response()->json(
-                ['message' => 'Некорректный формат токена'],
-                HttpStatus::BAD_REQUEST
-            );
+            return new ErrorResource(message: 'Некорректный формат токена', statusCode: HttpStatus::BAD_REQUEST);
         }
 
         // Извлекаем токен из заголовка
         $token = trim(substr($authHeader, 7)); // длина 'Bearer ' = 7
 
         if (!$token) {
-            return response()->json(
-                ['message' => 'Пустой токен'],
-                HttpStatus::UNAUTHORIZED
-            );
+            return new ErrorResource(message: 'Пустой токен', statusCode: HttpStatus::UNAUTHORIZED);
         }
 
         $user = User::where('api_token', $token)->first();
 
         if (!$user) {
-            return response()->json(
-                ['message' => 'Пользователь не найден или токен невалиден'],
-                HttpStatus::UNAUTHORIZED
-            );
+            return new ErrorResource(message: 'Пользователь не найден или токен невалиден', statusCode: HttpStatus::UNAUTHORIZED);
         }
 
         $request->setUserResolver(fn() => $user);
