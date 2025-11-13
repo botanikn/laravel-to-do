35928eb8432289fd39cb973c08dd02d626e28ae0
yusef.alassar <yusef.alassar@simpleone.ru>
2025-11-13 17:40:41 +0300

    task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,21 +9,42 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $text
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read \App\Models\User $user
+ * @property-read \Illuminate\Database\Eloquent\Collection<\App\Models\Tag> $tags
+ */
 class Task extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'title',
         'text',
-        'user_id'
+        'user_id',
     ];
 
+    /**
+     * Get the user that owns the task.
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Get the tags associated with the task.
+     */
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
